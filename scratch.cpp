f58e5c279a1504a201928dc6dbfb4e2e17697412
Nishant Pandey <118831586+nishantp22@users.noreply.github.com>
2023-04-14 16:41:55 +0530

    Add files via upload

--- scratch.cpp
+++ scratch.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int i=0;
+int op=0;
 class Car{
     public:
         string make="";
@@ -15,7 +16,8 @@ class Car{
         
         
         Car(){          //this is a constructor which sets the corresponding values given by user
-            cout<<"This is the "<<i+1<<"th car\n";
+            cout<<"\n";
+            cout<<"This is car no. "<<i+1<<"\n";
             cout<<"Enter the make of the car (e.g., Hyundai, Maruti, BMW) : ";
             cin>>make;
             cout<<"Enter the model of the car (e.g., Alto, Creta, City) : ";
@@ -26,52 +28,71 @@ class Car{
             cin>>x>>y>>z;
             cout<<"Enter the x,y,z componants of speed (separated by spaces): ";
             cin>>speed_x>>speed_y>>speed_z;
+            cout<<"\nCar successfully Added\n";
             i++;
         }
         
         
-        int menu(){     //menu which will perform different functions based on user's choice
-            cout<<"         Select a choice for the 1st car\n";
+        int menu(int i){     //menu which will perform different functions based on user's choice
+            cout<<"\n         Select a choice for car no. "<<i+1<<"\n";
             cout<<"1. Print the data\n";
             cout<<"2. Accelerate\n";
             cout<<"3. Decelerate\n";
             cout<<"4. Move\n";
-            cout<<"5. Check if the car has collided with 2nd car\n";
-            cout<<"6. Check the time of collision with 2nd car\n";
-            cout<<"7. Exit\n";
+            cout<<"5. Calculate the distance between two cars\n";
+            cout<<"6. Check if the car has collided with another car\n";
+            cout<<"7. Check the time before collision with another car\n";
             int choice;
             cin>>choice;
+            int idx;
             switch(choice){
                 case 1:
                     printCurrentData();
-                    return 1;
+                    op=1;
+                    return -1;
                 case 2:
                     int ax,ay,az;
                     cout<<"Enter increments in speeds(x,y,z, separated by spaces) : ";
                     cin>>ax>>ay>>az;
                     accelerate(ax,ay,az);
-                    return 1;
+                    op=2;
+                    return -1;
                 case 3:
                     int dx,dy,dz;
                     cout<<"Enter decrements in speeds(x,y,z, separated by spaces) : ";
                     cin>>dx>>dy>>dz;
                     decelerate(dx,dy,dz);
-                    return 1;
+                    op=3;
+                    return -1;
                 case 4:
                     move();
-                    return 1;
+                    op=4;
+                    return -1;
                 case 5:
-                    return 5;
+                    cout<<"Enter the numer of the second car(different from the first one, 1<=number<=n) : ";
+                    cin>>idx;
+                    op=5;
+                    return idx-1;
                 case 6:
-                    return 6;
+                    cout<<"Enter the number of the second car(different from the first one, 1<=number<=n) : ";
+                    cin>>idx;
+                    op=6;
+                    return idx-1;
                 case 7:
+                    cout<<"Enter the number of the second car(different from the first one, 1<=number<=n) : ";
+                    cin>>idx;
+                    op=7;
+                    return idx-1;
+                case 8:
                     cout<<"Bye :)";
-                    return 7;
+                    op=8;
+                    return -1;
             }
         }
         
         
         void printCurrentData(){        //method to print current data for a car    
+            cout<<"\n";
             cout<<"Model : "<<model<<endl;
             cout<<"Make : "<<make<<endl;
             cout<<"Year the car was made : "<<year<<endl;
@@ -84,7 +105,7 @@ class Car{
             this->speed_x+=inc_x;       //using this-> to increment the speed of that particular car
             this->speed_y+=inc_y;
             this->speed_z+=inc_z;
-            cout<<"\n           Successfully accelerated\n";
+            cout<<"\n           Successfully accelerated\n\n";
         }
         
         
@@ -92,7 +113,7 @@ class Car{
             this->speed_x-=dec_x;
             this->speed_y-=dec_y;
             this->speed_z-=dec_z;
-            cout<<"\n           Successfully decelerated\n";
+            cout<<"\n           Successfully decelerated\n\n";
         }
         
         
@@ -100,17 +121,22 @@ class Car{
             x+=speed_x;
             y+=speed_y;
             z+=speed_z;
-            cout<<"     Successfully moved\n";
+            cout<<"     Successfully moved\n\n";
         }
         
+        void distanceBetweenCars(Car c){
+            int d=pow((c.x-x),2)+pow((c.y-y),2)+pow((c.z-z),2);
+            float dst=sqrt(d);
+            cout<<"\n       The Distance between the two cars at this instant is : "<<dst<<" units\n\n";
+        }
         
         void detectCollision(Car c){        //method to detect if the two cars have collided
             if(c.x==x&&c.y==y&&c.z==z){
-                cout<<"         The two cars have collided :(\n";
+                cout<<"\n         The two cars have collided :(\n\n";
                 return;
             }
             else{
-                cout<<"         The two cars have not yet collided :)\n";
+                cout<<"\n         The two cars have not yet collided :)\n\n";
                 return;
             }
         }
@@ -122,51 +148,70 @@ class Car{
             int ry=c.y-y;
             int rz=c.z-z;
             if(rx==0&&ry==0&&rz==0){    //if relative distance is already zero then the cars will collide
-                cout<<"     The two cars have already collided :(\n";
+                cout<<"\n     The two cars have already collided :(\n\n";
                 return;
             }
             int vx=c.speed_x-speed_x;
             int vy=c.speed_y-speed_y;
             int vz=c.speed_z-speed_z;
             if(vx==0&&vy==0&&vz==0){    //if relative velocity is zero given relative distance !=0 then cars wont collide
-                cout<<"     The two cars will not collide at current positions and speeds:)\n";
+                cout<<"\n     The two cars will not collide based on current positions and speeds:)\n\n";
                 return;
             }
             int dot=rx*vx+ry*vy+rz*vz;
             int mag_r=rx*rx+ry*ry+rz*rz;
             int mag_v=vx*vx+vy*vy+vz*vz;
             if(dot==-sqrt(mag_r*mag_v)){  //checking if angle between relative distance and relative velociy is pi
-                cout<<"     The two cars will collide at current positions and speeds :(\n";
-                cout<<"     The time of collision is : "<<sqrt((float)(mag_r)/(float)mag_v)<<" seconds\n";
-                 cout<<"     The distance of collision from the second car is : "<<mag_v*sqrt((float)(mag_r)/(float)mag_v)<<" units\n";
+                cout<<"\n     The two cars will collide based on current positions and speeds :(\n";
+                cout<<"\n     The time before collision is : "<<sqrt((float)(mag_r)/(float)mag_v)<<" seconds\n\n";
                 //the above line prints the distance of collision from the second car
                 return;
 
             }
             else{
-                cout<<"     The cars will not collide at current positions and speeds :)";
+                cout<<"\n     The cars will not collide based on current positions and speeds :)\n\n";
                 return;
             }
        }
 };
 int main(){
-    Car c1,c2;
+    vector<Car>Cars;
+    int n=0;
+    cout<<"Enter Number of cars : ";
+    cin>>n;
+    while(n--){
+        Car c;              //creating n Car objects and storing them in a vector
+        Cars.push_back(c);
+    }
     int q=0;
     while(1){
         cout<<"\n";
-        int t=c1.menu();
-        switch(t){
+        int id;
+        int y=0;
+        cout<<"Do you wanna analyse the cars? (Type 1 for YES and 0 for NO) : ";
+        cin>>y;
+        if(y!=0&&y!=1){
+            cout<<"Invalid response :(\n";
+            continue;
+        }
+        else if(y==0) {
+            cout<<"\n       Okay, Bye :)";
+            break;
+        }
+        cout<<"Enter the number of a car you wanna analyse(1<=number<=n) : ";
+        cin>>id;
+        int t=Cars[id-1].menu(id-1);
+        switch(op){
             case 5:
-                c1.detectCollision(c2);
+                Cars[id-1].distanceBetweenCars(Cars[t]);
                 break;
             case 6:
-                c1.timeToCollision(c2);
+                Cars[id-1].detectCollision(Cars[t]);
                 break;
-            case 7:         //exiting the loop if the user does not want to proceed further
-                q=1;
+            case 7:
+                Cars[id-1].timeToCollision(Cars[t]);
                 break;
         }
-        if(q==1) break;
     }
     return 0;
 }
